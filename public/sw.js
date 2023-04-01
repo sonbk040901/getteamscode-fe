dacd65a3a8afdac96c79a364a1fd6f17da316258
sonbk040901 <leducson007@gmail.com>
2023-04-01 21:24:57 +0700

    v1.0.1

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `getteamscode-v1`;
+const CACHE_NAME = `getteamscode-v1.0.2`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener("install", (event) => {
@@ -32,5 +32,19 @@ self.addEventListener("fetch", (event) => {
         }
       }
     })()
+  );
+});
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
